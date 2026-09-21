5a8c5062f1edd6db8579e7bd525d71051bfc1fe3
lekienvn7 <ltrgkien68.68@gmail.com>
2026-09-21 21:47:52 +0700

    21.9 - v0.1.2 : Xử lý button với led

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <Arduino.h>
 
 #define LED_PIN 18
+#define BUTTON_PIN 23
 
 void setup() {
-
   pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-
-  digitalWrite(LED_PIN, HIGH);
-  delay(200);
-
-  digitalWrite(LED_PIN, LOW);
-  delay(200);
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    digitalWrite(LED_PIN, HIGH);
+  } 
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
